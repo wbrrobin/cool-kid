79e4eb8bb54b74214875067aefe9aafecf02a9ad
wbrrobin <weber_ro@gmx.de>
2025-09-25 16:25:09 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField, REST, Routes } = require('discord.js');
 const eventHandler = require("./handlers/eventHandler");
-const { token } = require("../config.json");
+const { clientId, guildId, token } = require("../config.json");
+
+const rest = new REST().setToken(token);
+
+// ...
+
+// for guild-based commands
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
+	.catch(console.error);
+
+// for global commands
+rest.put(Routes.applicationCommands(clientId), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
+	.catch(console.error);
+
 
 // Bot instance
 const client = new Client({
