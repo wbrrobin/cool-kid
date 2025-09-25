ae4f7545e280a3ac31b7e4900c6b6ac4bac1fe26
wbrrobin <weber_ro@gmx.de>
2025-09-25 22:46:46 +0200

    fix mogi and add war

---
+++ src/commands/misc/mogi.js
@@ -0,0 +1,39 @@
+const { ApplicationCommandOptionType, BaseClient } = require("discord.js");
+
+module.exports = {
+    name: "mogi",
+    description: "Creates a new mogi voting for a given mogi number.",
+    options: [
+        {
+            name: "number",
+            description: "Number of the mogi in #lounge-sq-schedule.",
+            type: ApplicationCommandOptionType.Number,
+            required: true
+        }
+    ],
+    callback: async (client, interaction) => {
+        try {
+            const mogiNr = interaction.options.get("number").value;
+
+            const channel = await client.channels.fetch("1399040539109756938");
+            const messages = await channel.messages.fetch({ limit: 1 });
+            const lastMessage = messages.first().content;
+
+            const lines = lastMessage.split("\n");
+            let mogiDetails = lines.find((line) => line.includes(`#${mogiNr}`));
+            mogiDetails = mogiDetails.split(" - ")[0];
+
+            await interaction.deferReply();
+            interaction.deleteReply();
+            interaction.channel
+                .send(`<@&${"1396340893476196432"}> React to this if you could mogi ${mogiDetails}`)
+                .then(message => {
+                    message.react("✅");
+                    message.react("🔁");
+                    message.react("❌");
+                });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
--- src/commands/misc/war.js
+++ src/commands/misc/war.js
@@ -18,7 +18,7 @@ module.exports = {
         },
         {
             name: "3_time",
-            description: "Time of the war.",
+            description: "Time of the war. (bbs timezone lol)",
             type: ApplicationCommandOptionType.Number,
             required: true,
             choices: Array.from({ length: 24 }, (_, i) => {
@@ -30,23 +30,26 @@ module.exports = {
         }
     ],
     callback: async (client, interaction) => {
-        const month = interaction.options.get("1_month").value;
-        const day = interaction.options.get("2_day").value;
-        const hour = interaction.options.get("3_time").value;
+        try {
+            const month = interaction.options.get("1_month").value;
+            const day = interaction.options.get("2_day").value;
+            const hour = interaction.options.get("3_time").value;
 
-        const year = 2025;
+            const year = 2025;
 
-        const date = new Date(Date.UTC(year, month - 1, day, hour));
+            const date = new Date(Date.UTC(year, month - 1, day, hour));
 
-        const roleId = "1396340893476196432"
-        await interaction.deferReply();
-        interaction.deleteReply();
-        interaction.channel
-            .send(`<@&${roleId}> React to this if you could war <t:${date.getTime() / 1000 - 7200}:F>`)
-            .then(message => {
-                message.react("✅");
-                message.react("🔁");
-                message.react("❌");
-            });
+            await interaction.deferReply();
+            interaction.deleteReply();
+            interaction.channel
+                .send(`<@&${"1396340893476196432"}> React to this if you could war <t:${date.getTime() / 1000 - 9 * 3600}:F>`)
+                .then(message => {
+                    message.react("✅");
+                    message.react("🔁");
+                    message.react("❌");
+                });
+        } catch (error) {
+            console.log(error);
+        }
     }
 };
