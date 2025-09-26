b7fdda6cc0d58f9cffe2d4185620a4f3b0e63781
wbrrobin <weber_ro@gmx.de>
2025-09-26 18:06:55 +0200

    fix gl

--- src/commands/misc/goodLuck.js
+++ src/commands/misc/goodLuck.js
@@ -1,9 +1,9 @@
-const { guildId } = require("../../../config.json");
+const { guildId, testGuildId } = require("../../../config.json");
 
 module.exports = {
     name: 'goodluck',
     description: 'Sends a good luck message.',
-    deleted: "952564259047501834" !== guildId,
+    deleted: guildId !== testGuildId,
     callback: async (client, interaction) => {
         const channel = await client.channels.fetch("1412221455038550107");
         channel.send("GOOD LUCK IN WAR EVERYONE! 🍀");
